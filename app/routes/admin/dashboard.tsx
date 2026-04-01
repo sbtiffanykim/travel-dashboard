03064dabd7573069248703c1fe5de22d2d460e1c
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2026-04-01 15:05:25 +0900

    add a header

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import { Header } from 'components';
 
 const Dashboard = () => {
-  return <div>dashboard page</div>;
+  // fake user
+  const user = {
+    name: 'abc',
+  };
+
+  return (
+    <main className='dashboard wrapper'>
+      <Header
+        title={`Welcome ${user?.name ?? 'Guest'} 👋`}
+        description='Track activity, trends and popular destinations in real time'
+      />
+      Dashboard Page Contents
+    </main>
+  );
 };
 
 export default Dashboard;
